ac818a9da85dbc23fed66492cd7439b2c0123ebb
eh0203 <brianj0203@hotmail.com>
2022-05-09 11:36:16 +0900

    Snake#length 추가

--- hello_git/src/com/kh/animal/Snake.java
+++ hello_git/src/com/kh/animal/Snake.java
@@ -1,8 +1,9 @@
 package com.kh.animal;
 
 public class Snake {
-	
+	private double length; //master 추가
 	private String name; // master에서 추가
+
 	public void move() {
 		System.out.println("Ssssss");
 	}
