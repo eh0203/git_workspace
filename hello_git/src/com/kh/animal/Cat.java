ed318cf3c4e9ad41b12872a41bc14dce3f2ff746
eh0203 <brianj0203@hotmail.com>
2022-05-09 12:26:10 +0900

    Cat#breed 추가

--- hello_git/src/com/kh/animal/Cat.java
+++ hello_git/src/com/kh/animal/Cat.java
@@ -3,7 +3,8 @@ package com.kh.animal;
 public class Cat {
 	private String name; // master 작업
 	private String color; //animal 추가
-
+	private String breed;
+	
 	public void cry() {
 		System.out.println("냥");
 	}
