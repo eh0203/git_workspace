ac89598f43d60f10966167850a64646a412dd0d8
eh0203 <brianj0203@hotmail.com>
2022-05-09 10:36:00 +0900

    dog객체의 bark 호출

--- hello_git/src/com/kh/git/Helloworld.java
+++ hello_git/src/com/kh/git/Helloworld.java
@@ -1,8 +1,13 @@
 package com.kh.git;
 
+import com.kh.animal.Dog;
+
 public class Helloworld {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+		
+		Dog dog = new Dog();
+		dog.bark();
 	}
 
 }
