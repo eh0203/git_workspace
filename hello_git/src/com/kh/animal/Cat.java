5dbcd95694c2536ee2deadbb7d2e53a1d74f0b92
eh0203 <33949767+eh0203@users.noreply.github.com>
2022-05-09 13:36:03 +0900

    Update Cat.java

--- hello_git/src/com/kh/animal/Cat.java
+++ hello_git/src/com/kh/animal/Cat.java
@@ -4,6 +4,7 @@ public class Cat {
 	private String name; // master 작업
 	private String color; //animal 추가
 	private String breed;
+	private int age;
 	
 	public void cry() {
 		System.out.println("냥");
